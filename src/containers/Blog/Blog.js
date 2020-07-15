3f8ee64b97b073ad7eea8619cf2b8ca3dee97023
renee.llup <renee.llup@gmail.com>
2020-07-15 18:03:06 +0800

    164. Transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import post from "../../components/Post/Post";
 
 class Blog extends Component {
   state = {
@@ -12,13 +13,20 @@ class Blog extends Component {
   };
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      this.setState({ posts: response.data });
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Renee L. Llup",
+        };
+      });
+      this.setState({ posts: updatedPosts });
       //   console.log(response);
     });
   }
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return <Post key={post.id} title={post.title} author={post.author} />;
     });
     return (
       <div>
